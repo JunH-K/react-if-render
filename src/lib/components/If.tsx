f4cbbd66f011c310b0caf0695f69467bbd26322f
JunH-K <k.developer@kakao.com>
2023-11-06 09:45:19 +0900

    ♻️ map callback funtion 분리

--- src/lib/components/If.tsx
+++ src/lib/components/If.tsx
@@ -1,38 +1,33 @@
-import { Fragment, Children, isValidElement } from "react";
+import { Fragment, Children, isValidElement, ReactNode } from "react";
 import { ELSE, THEN, WARN_MESSAGE } from "../constant";
 
 type Props = {
   condition: boolean;
-  children?: React.ReactNode;
+  children?: ReactNode;
 };
 
 function If({ condition, children }: Props) {
-  return (
-    <Fragment>
-      {Children.map(children, (child) => {
-        if (!isValidElement(child)) return handleInvalidChild();
-        if (typeof child.type === "string") return handleInvalidChild();
-        if (!("displayName" in child.type)) return handleInvalidChild();
-        if (typeof child.type.displayName !== "string")
-          return handleInvalidChild();
+  const processChild = (child: ReactNode) => {
+    if (!isValidElement(child)) return handleInvalidChild();
+    if (typeof child.type === "string") return handleInvalidChild();
+    if (!("displayName" in child.type)) return handleInvalidChild();
+    if (typeof child.type.displayName !== "string") return handleInvalidChild();
 
-        const { displayName } = child.type;
-        const isThen = condition && displayName === THEN;
-        const isElse = !condition && displayName === ELSE;
-        const isExtra = ![THEN, ELSE].includes(displayName);
+    const { displayName } = child.type;
+    const isThen = condition && displayName === THEN;
+    const isElse = !condition && displayName === ELSE;
+    const isExtra = ![THEN, ELSE].includes(displayName);
 
-        if (isThen) {
-          return child;
-        } else if (isElse) {
-          return child;
-        } else if (isExtra) {
-          console.warn(WARN_MESSAGE);
-          return null;
-        }
-        return null;
-      })}
-    </Fragment>
-  );
+    if (isThen || isElse) {
+      return child;
+    } else if (isExtra) {
+      console.warn(WARN_MESSAGE);
+      return null;
+    }
+    return null;
+  };
+
+  return <Fragment>{Children.map(children, processChild)}</Fragment>;
 }
 
 const handleInvalidChild = () => {

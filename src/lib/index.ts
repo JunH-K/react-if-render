d59543ba54293af381f76ccf76d6b78c3cca9bee
JunH-K <k.developer@kakao.com>
2023-11-08 23:35:02 +0900

    🚑️ export 누락

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 export { default as If } from "./components/If";
 export { default as Then } from "./components/Then";
 export { default as Else } from "./components/Else";
+export { default as When } from "./components/When";
+export { default as Unless } from "./components/Unless";
